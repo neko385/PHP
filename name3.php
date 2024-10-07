f6c34e19bd676e0f4e6184e64a076146204238c6
Tanaka <102905708+neko385@users.noreply.github.com>
2024-10-07 15:52:01 +0900

    Chapter3 演習問題

---
+++ name3.php
@@ -0,0 +1,6 @@
+<?php 
+
+$family_name = "Yamada";
+$first_name ="Tarou";
+
+echo "{$family_name} {$first_name} \n";
---
+++ price.php
@@ -0,0 +1,5 @@
+<?php 
+
+$price = 1800;
+$price_with_tax = $price * 1.10;
+echo "{$price_with_tax} \n";
