e177c8dc6d23965c45b5eae752d33c53ef0c8e9c
Tanaka <102905708+neko385@users.noreply.github.com>
2024-10-08 17:27:13 +0900

    chapter8 演習問題

---
+++ chapter8_1.php
@@ -0,0 +1,13 @@
+<?php 
+
+$array = [
+    "Red",
+    "Yellow",
+    "Green",
+    "Blue",
+    "Black",
+];
+
+foreach($array as $v) {
+    echo "{$v} \n";
+}
---
+++ chapter8_2.php
@@ -0,0 +1,13 @@
+<?php 
+
+$colors = [
+        "Red" => "#ff0000",
+        "Yellow" => "#ffff00",
+        "Green" => "#00ff00",
+        "Blue" => "#0000ff",
+        "Blavk" => "#000000",
+];
+
+foreach($colors as $k => $v) {
+    echo "{$k}: {$v} \n";
+}
